3743032a6f759bab8320a7ccec80f09e030c1808
Bastien Fournier <bastien.fournier06@gmail.com>
2024-05-21 14:50:18 +0200

    fix conneries

--- server/index.js
+++ server/index.js
@@ -1,22 +1,13 @@
-// Load environment variables from .env file
 require("dotenv").config();
+const express = require("express");
 
-// Check database connection
-// Note: This is optional and can be removed if the database connection
-// is not required when starting the application
-require("./database/client").checkConnection();
-
-// Import the Express application from app/config.js
-const app = require("./app/config");
-
-// Get the port from the environment variables
+const app = express();
 const port = process.env.APP_PORT;
 
-// Start the server and listen on the specified port
-app
-  .listen(port, () => {
-    console.info(`Server is listening on port ${port}`);
-  })
-  .on("error", (err) => {
-    console.error("Error:", err.message);
-  });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.info(`Server is running on http://localhost:${port}`);
+});
